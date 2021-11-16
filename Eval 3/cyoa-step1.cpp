84aa1f233e468f3deb6f2177d0dc91a7f7df1cd6
Erica233 <64255604+Erica233@users.noreply.github.com>
2021-11-15 23:55:39 -0500

    Update cyoa-step1.cpp

--- Eval 3/cyoa-step1.cpp
+++ Eval 3/cyoa-step1.cpp
@@ -1,17 +1,13 @@
-#include <cstdlib>
-#include <fstream>
-#include <iostream>
-
 #include "page.hpp"
 
 int main(int argc, char ** argv) {
-  //reference: MLP079
   if (argc != 2) {
     std::cerr << "Usage: program inputFile\n";
     return EXIT_FAILURE;
   }
   //store page
   Page page(argv[1]);
-  
+  page.store_story(argv[1]);
+  std::cout << page;
   return EXIT_SUCCESS;
 }
