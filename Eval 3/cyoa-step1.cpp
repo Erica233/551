30a7e8541cc4f8a01bf5f4cb1bcf8e33cda4d985
Erica233 <64255604+Erica233@users.noreply.github.com>
2021-11-17 12:14:18 -0500

    Update cyoa-step1.cpp

--- Eval 3/cyoa-step1.cpp
+++ Eval 3/cyoa-step1.cpp
@@ -5,9 +5,10 @@ int main(int argc, char ** argv) {
     std::cerr << "Usage: program inputFile\n";
     return EXIT_FAILURE;
   }
+
   //store and print page
   Page page(argv[1]);
-  page.store_page(argv[1]);
+  page.store_page();
   std::cout << page;
   return EXIT_SUCCESS;
 }
