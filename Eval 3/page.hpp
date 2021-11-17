39c8fd998358cf27594cc465837c9ac625e407c5
Erica233 <64255604+Erica233@users.noreply.github.com>
2021-11-17 10:49:45 -0500

    Update page.hpp

--- Eval 3/page.hpp
+++ Eval 3/page.hpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -8,16 +9,56 @@
 
 class Page {
  private:
+  int page_num;
+  std::string path;
+  std::string dir_name;
   std::string page_name;
   std::vector<std::string> navigator;
   std::vector<std::string> text;
 
  public:
-  Page(char * filename) : page_name(filename) {}
-  void store_story(char * filename) {
+  //explicit Page(int n) : page_num(n) {}
+  explicit Page(char * filename) : path(filename) {}
+  //void read_story(){}
+  void store_page(char * filename) {
+    //store path
+    //reference: string::find in cplusplus.com
+    std::cout << "path:" << path << std::endl;
+    std::size_t found;
+    std::string num;
+    std::string former_part("/page");
+    std::string latter_part(".txt");
+    found = path.find(former_part);
+    //found = path.find("/path");
+    if (found != std::string::npos) {
+      dir_name = path.substr(0, found);
+      page_name = path.substr(found + 1);
+      std::cout << "dir_name: " << dir_name << "\npage_name: " << page_name << std::endl;
+    }
+    else {
+      std::cout << "part1\n";
+      std::cerr << "invalid input file name\n";
+      exit(EXIT_FAILURE);
+    }
+    std::size_t found_tail = path.find(latter_part);
+    if (found_tail != std::string::npos) {
+      num = path.substr(found + former_part.length(),
+                        found_tail - found - former_part.length());
+      std::cout << "num: " << num << std::endl;
+    }
+    else {
+      std::cerr << "invalid input file name\n";
+      exit(EXIT_FAILURE);
+    }
+    std::stringstream num_ss(num);
+    num_ss >> page_num;
+    std::cout << "page_num:" << page_num << std::endl;
     //reference: MLP079_sort_cpp
     std::ifstream file;
-    file.open(filename);
+    //reference: string::c_str in cplusplus.com
+    char * cpath = new char[path.length() + 1];
+    std::strcpy(cpath, path.c_str());
+    file.open(cpath);
     if (file.is_open()) {
       std::string line;
       std::vector<std::string> lines;
@@ -41,6 +82,7 @@ class Page {
       exit(EXIT_FAILURE);
     }
     file.close();
+    delete[] cpath;
   }
 
   friend std::ostream & operator<<(std::ostream & stream, const Page & page);
