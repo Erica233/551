766cc7cd8dd2d1f896e8da8ca8e5bffdfd6ee984
Erica233 <64255604+Erica233@users.noreply.github.com>
2021-11-30 12:22:47 -0500

    Create cyoa-step4.cpp

---
+++ Eval 3/cyoa-step4.cpp
@@ -0,0 +1,20 @@
+#include "story.hpp"
+
+int main(int argc, char ** argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: program inputFile\n";
+    return EXIT_FAILURE;
+  }
+  //store story
+  Story story(argv[1]);
+  story.store_story();
+  story.check_valid_story();
+
+  //find all the cycle-free winning paths
+  
+  
+  //determine story depth
+  //story.cal_depth();
+
+  return EXIT_SUCCESS;
+}
